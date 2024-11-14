3fee520b31b84ea3fd31d0b978cf7d0a336bdeda
manikerisaurabh <manikerisaurabh@gmail.com>
2024-11-14 23:30:01 +0530

    all done

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,44 @@
+
+
+import { redis } from '@/lib/redis';
+import { ragChat } from '@/lib/reg-chat'
+import React from 'react'
+import { ChatWrapper } from '../components/ChatWrapper';
+import { cookies } from 'next/headers';
+interface PageProps {
+    params: {
+        url: string | string[] | undefined
+
+    }
+}
+
+function reconstructUrl({ url }: { url: string[] }) {
+    const decodedComponents = url.map((component) => decodeURIComponent(component));
+
+    return decodedComponents.join('/')
+}
+const Page = async ({ params }: PageProps) => {
+    const sessionCookie = (await cookies()).get("sessionId")?.value;
+    const reconstructedUrl = reconstructUrl({ url: params.url as string[] })
+
+    const sessionId = (reconstructedUrl + "--" + sessionCookie).replace(/\//g, "")
+
+    const isAlreadyIndexe = await redis.sismember("indexed-urls", reconstructedUrl);
+
+    const initialMessages = await ragChat.history.getMessages({ amount: 10, sessionId }) //amount is to get last 10 messages we can increase that 
+    if (!isAlreadyIndexe) {
+
+        await ragChat.context.add({
+            type: "html",
+            source: reconstructedUrl,
+            config: { chunkOverlap: 50, chunkSize: 200 }
+        })
+
+        await redis.sadd("indexed-urls", reconstructedUrl);
+    }
+    return (
+        <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+    )
+}
+
+export default Page
